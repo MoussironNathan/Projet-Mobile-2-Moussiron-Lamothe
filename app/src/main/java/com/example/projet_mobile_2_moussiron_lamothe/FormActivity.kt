203cf73d8bdd8754a017111e4b608eea24f6b7d7
MoussironNathan <nathan.moussiron@gmail.com>
2023-04-16 15:47:06 +0200

    ajout qrcode + phase connexion

--- app/src/main/java/com/example/projet_mobile_2_moussiron_lamothe/FormActivity.kt
+++ app/src/main/java/com/example/projet_mobile_2_moussiron_lamothe/FormActivity.kt
@@ -2,9 +2,12 @@ package com.example.projet_mobile_2_moussiron_lamothe
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.core.content.ContextCompat
+import org.json.JSONObject
 
 class FormActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +17,42 @@ class FormActivity : BaseActivity() {
         setHeaderTxt("Création de compte")
         showBack()
 
+        val resultScan = intent.extras!!.getString("result","")
+        val donnees = JSONObject(resultScan)
+
+        val editTextLastName = findViewById<EditText>(R.id.editTextLastName)
+        editTextLastName.setText(donnees.get("lastName").toString())
+
+        val editTextFirstName = findViewById<EditText>(R.id.editTextFirstName)
+        editTextFirstName.setText(donnees.get("firstName").toString())
+
+        val editTextEmailAddress = findViewById<EditText>(R.id.editTextEmailAddress)
+        editTextEmailAddress.setText(donnees.get("email").toString())
+
+        val editTextAddress = findViewById<EditText>(R.id.editTextAddress)
+        editTextAddress.setText(donnees.get("address").toString())
+
+        val editTextCodePostal = findViewById<EditText>(R.id.editTextCodePostal)
+        editTextCodePostal.setText(donnees.get("zipcode").toString())
+
+        val editTextCity = findViewById<EditText>(R.id.editTextCity)
+        editTextCity.setText(donnees.get("city").toString())
+
+        val editTextCardRef = findViewById<EditText>(R.id.editTextCodeCarte)
+        editTextCardRef.setText(donnees.get("cardRef").toString())
+
+        val button = findViewById<Button>(R.id.signUpButtonSignUp)
+        button.setOnClickListener{
+            (application as MyApplication).writeSharedPref("lastName", editTextLastName.text.toString())
+            (application as MyApplication).writeSharedPref("firstName", editTextFirstName.text.toString())
+            (application as MyApplication).writeSharedPref("email", editTextEmailAddress.text.toString())
+            (application as MyApplication).writeSharedPref("address", editTextAddress.text.toString())
+            (application as MyApplication).writeSharedPref("zipcode", editTextCodePostal.text.toString())
+            (application as MyApplication).writeSharedPref("city", editTextCity.text.toString())
+            (application as MyApplication).writeSharedPref("cardRef", editTextCardRef.text.toString())
+
+            val intent = Intent(application, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/projet_mobile_2_moussiron_lamothe/SignInActivity.kt
+++ app/src/main/java/com/example/projet_mobile_2_moussiron_lamothe/SignInActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
 
 class SignInActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +18,7 @@ class SignInActivity : BaseActivity() {
         val buttonForm = findViewById<Button>(R.id.buttonForm)
 
         buttonQR.setOnClickListener(View.OnClickListener {
-            val intent = Intent(application, QRcodeActivity::class.java)
-            startActivity(intent)
+            IntentIntegrator(this).initiateScan()
         })
 
         buttonForm.setOnClickListener(View.OnClickListener {
@@ -25,5 +26,19 @@ class SignInActivity : BaseActivity() {
             startActivity(intent)
         })
 
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                val intent = Intent(application, FormActivity::class.java)
+                intent.putExtra("result", result.contents.toString())
+                startActivity(intent)
+            }
+        }
     }
 }
