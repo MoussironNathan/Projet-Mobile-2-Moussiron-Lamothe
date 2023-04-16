8d1012dde7065feb16aa7e6e2fb63809494ad7e1
hugo-lamothe <hugo.lamothe@ecoles-wis.net>
2023-04-16 14:32:05 +0200

    scan qrcode

--- app/src/main/java/com/example/projet_mobile_2_moussiron_lamothe/QRcodeActivity.kt
+++ app/src/main/java/com/example/projet_mobile_2_moussiron_lamothe/QRcodeActivity.kt
@@ -1,18 +1,42 @@
 package com.example.projet_mobile_2_moussiron_lamothe
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.Result
+import me.dm7.barcodescanner.zxing.ZXingScannerView
 
-class QRcodeActivity : BaseActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.qrcode_activity)
+/**
+ * Created by Parsania Hardik on 19-Mar-18.
+ */
+class QRcodeActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
 
-        setHeaderTxt("Création de compte")
-        showBack()
+    private var mScannerView: ZXingScannerView? = null
 
+    public override fun onCreate(state: Bundle?) {
+        super.onCreate(state)
+        mScannerView = ZXingScannerView(this)   // Programmatically initialize the scanner view
+        setContentView(mScannerView)                // Set the scanner view as the content view
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        mScannerView!!.setResultHandler(this) // Register ourselves as a handler for scan results.
+        mScannerView!!.startCamera()          // Start camera on resume
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        mScannerView!!.stopCamera()           // Stop camera on pause
+    }
+
+    override fun handleResult(rawResult: Result) {
+        // Do something with the result here
+        // Log.v("tag", rawResult.getText()); // Prints scan results
+        // Log.v("tag", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+
+        onBackPressed()
+
+        // If you would like to resume scanning, call this method below:
+        //mScannerView.resumeCameraPreview(this);
     }
 }
