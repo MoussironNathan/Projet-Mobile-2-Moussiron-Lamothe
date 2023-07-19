2409ab908f6d591c090f6e279257d47742e54c08
MoussironNathan <nathan.moussiron@gmail.com>
2023-07-19 15:10:41 +0200

    update

--- app/src/main/java/com/example/projet_mobile_2_moussiron_lamothe/DetailMagasinActivity.kt
+++ app/src/main/java/com/example/projet_mobile_2_moussiron_lamothe/DetailMagasinActivity.kt
@@ -22,24 +22,19 @@ class DetailMagasinActivity : BaseActivity() {
 
         if(intent.extras!=null){
             val title=intent.extras!!.getString("name","XXX")
-            println(title)
             setHeaderTxt(title)
 
             val url=intent.extras!!.getString("urlImage","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRj0AEwRdUSWfs2LPDlLKn9kI-KvverDKfy0w&usqp=CAU")
-            println(url)
             Picasso.get().load(url).into(image)
 
             val addr=intent.extras!!.getString("adresse","XXX")
-            println(addr)
             address.text = addr
 
             val city=intent.extras!!.getString("city","XXX")
             val zipcode=intent.extras!!.getString("zipcode","XXX")
-            println("$city $zipcode")
             cityTextView.text = "$city $zipcode"
 
             val desc=intent.extras!!.getString("description","XXX")
-            println(desc)
             description.text = desc
         }
 
